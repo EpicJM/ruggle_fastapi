a902615dc9568dcb56b1daa0c55d40199757273a
EpicJM <EpicJM@users.noreply.github.com>
2022-09-14 04:17:23 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="EpicJM"
+original_name="ruggle_fastapi"
+original_urlname="ruggle_fastapi"
+original_description="Awesome ruggle_fastapi created by EpicJM"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv ruggle_fastapi $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
